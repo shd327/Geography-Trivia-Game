72dee9f4a8a580a50c61197903ad176b44aaee32
Hiro Protagonist <shd327@gmail.com>
2022-10-31 16:05:27 -0600

    got answers and questions to display in html via js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const startButton = document.getElementById('start-btn');
-
+const questionElement = document.getElementById('questions')
+const answerElements = document.getElementById('answers-btns')
 const questionContainer = document.getElementById('question-container');
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 
+var randomizeQuestions;
+var currentQuestionIndex;
 
 const questions = [
     {
@@ -27,20 +30,39 @@ const questions = [
     }
 ]
 
-
+function shuffle() {
+    randomizeQuestions = questions.sort(() => Math.random() - .5)
+    console.log(randomizeQuestions)
+    currentQuestionIndex = 0;
+}
 
 function startGame() {
      console.log("hello")
     startButton.classList.add('hide')
     questionContainer.classList.remove('hide')
-   
+    shuffle()
+   setNextQuestion()
 }
 
 function setNextQuestion() {
-    
+    displayQuestions(randomizeQuestions[currentQuestionIndex])
 }
 
 function selectAnswer() {
     
 }
 
+function displayQuestions(question) {
+    questionElement.innerText = question.question
+    displayAnswers(question)
+    
+}
+function displayAnswers(question) {
+    question.answers.forEach(answer => {
+       const button = document.createElement('button');
+       button.innerText = answer.choice
+          button.classList.add('btn') 
+        answerElements.appendChild(button)
+        button.addEventListener('click', displayAnswers)
+   });
+}
