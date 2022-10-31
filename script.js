6095bfb31e31244e2b22da7f2736d8a0a96a3eca
Hiro Protagonist <shd327@gmail.com>
2022-10-31 15:22:41 -0600

    added element selectors

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const startButton = document.getElementById("start-btn")
+const startButton = document.getElementById('start-btn');
+const question = document.getElementById('questions')
+console.log(startButton);
+const questionContainer = document.getElementById('question-container');
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+     console.log("hello")
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide')
+   
+}
+
+function setNextQuestion() {
+    
+}
+
+function selectAnswer() {
+    
+}
